0f0ec0c77a49bf0cd4f577ffc056ba99cc4c41c6
Reid Sommerville <reid.sommerville@gmail.com>
2015-05-14 21:21:31 +0200

    adding a test java file

---
+++ src/com/testproj/app/App.java
@@ -0,0 +1,14 @@
+package com.testproj.app;
+
+public class App {
+
+	public App() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
